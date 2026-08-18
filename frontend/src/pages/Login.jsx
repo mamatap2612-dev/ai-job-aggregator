08d35d6f4376c44f2c1e04839928c347c3946c49
mamatap2612-dev <mamatap2612@gmail.com>
2026-08-18 18:02:08 +0530

    fix login api url

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,13 +7,18 @@ function Login({ onLoginSuccess, switchToRegister }) {
   const [message, setMessage] = useState('');
 
   const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('http://localhost:5000/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
-      });
+  e.preventDefault();
+  try {
+    const BASE_URL =
+      import.meta.env.VITE_API_BASE_URL ||
+      process.env.REACT_APP_API_URL ||
+      "http://localhost:5000";
+
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    });
 
       const data = await response.json();
 
